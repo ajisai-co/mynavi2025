b26721a5dcef34cd15f6bca1784bfb491d8fe9c5
shoji <shoji@ajisai-co.com>
2023-11-30 08:22:53 +0900

    CSS作成中

--- work/dist/assets/js/init.js
+++ work/dist/assets/js/init.js
@@ -1,42 +1,46 @@
 
-// swiper jsをつかったスライダーのサンプル
-var swiper_thumb = new Swiper(".js-thumbSlide", {
-	loop: false,
-	spaceBetween: 32,
-	slidesPerView: 4,
-	speed: 1000,
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-	breakpoints: {
-		769: {
-			spaceBetween: 32,
-			slidesPerView: 4,
+
+document.addEventListener('DOMContentLoaded', function () {
+	var swiper_thumb = new Swiper(".js-thumbSlide", {
+		loop: false,
+		spaceBetween: 32,
+		slidesPerView: 1,
+		centeredSlides: true,
+		speed: 1000,
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
 		},
-	},
-});
-var swiper_main = new Swiper(".js-mainSlide", {
-	effect: 'fade',
-	fadeEffect: {
-	  crossFade: true,
-	},
-	loop: false,
-	speed: 1000,
-	spaceBetween: 10,
-	slidesPerView: 1,
-	centeredSlides: true,
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-	breakpoints: {
-		769: {
-			spaceBetween: 20,
-			slidesPerView: 1,
-			scrollbar: {
-			dragSize: '267',
+		breakpoints: {
+			769: {
+				centeredSlides: false,
+				spaceBetween: 32,
+				slidesPerView: 4,
 			},
 		},
-	},
-});
+	});
+	var swiper_main = new Swiper(".js-mainSlide", {
+		effect: 'fade',
+		fadeEffect: {
+		crossFade: true,
+		},
+		loop: false,
+		speed: 1000,
+		spaceBetween: 10,
+		slidesPerView: 1,
+		centeredSlides: true,
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+		breakpoints: {
+			769: {
+				spaceBetween: 20,
+				slidesPerView: 1,
+				scrollbar: {
+				dragSize: '267',
+				},
+			},
+		},
+	});
+})
